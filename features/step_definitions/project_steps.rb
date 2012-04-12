fc20041ae581c2338ef1e2290bd032cf9d5fefc3
Francisco F <francisco000@gmail.com>
2012-04-11 23:52:02 -0300

    index action and view, factory para project

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -23,3 +23,7 @@ Then /^I should be on the project page for "([^"]*)"$/ do |arg1|
   current_path.should == project_path(Project.find_by_name!(arg1))
   page.should have_content(arg1+" - Projects - Ticketee")
 end
+
+Given /^there is a project called "([^"]*)"$/ do |name|
+  FactoryGirl.create(:project, :name => name)
+end
