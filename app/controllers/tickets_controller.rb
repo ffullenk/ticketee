db9ace642c3eaf9329d77cdb69f34b41369c1f92
Francisco F <francisco000@gmail.com>
2012-04-18 21:01:14 -0300

    edit ticket feature done

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,6 +16,20 @@ class TicketsController < ApplicationController
     #@ticket = Tickets.find(params[:id])
   end
   
+  def edit
+    
+  end
+
+  def update
+    
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"  
+    end
+  end
   def create
     
     @ticket = @project.tickets.build(params[:ticket])
