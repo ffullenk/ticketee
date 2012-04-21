7613ead6896c32cf0c2c1d1985780dfc0c974af5
Francisco F <francisco000@gmail.com>
2012-04-21 15:09:46 -0300

    tickets ahora estan correspondidos con el usuario creador

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,14 @@ Ticketee::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 465,
+  :user_name            => 'test@trocar.cl',
+  :password             => 'Desde345',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -7,4 +7,11 @@ end
 
 Then /^I should see "([^"]*)" within "([^"]*)"$/ do |arg1, arg2|
   page.should have_content(arg1) and have_content(arg2)
+end
+
+Given /^"([^\"]*)" has created a ticket for this project:$/ do |email, table|
+  table.hashes.each do |attributes|
+    attributes = attributes.merge!(:user => User.find_by_email!(email))
+    @project.tickets.create!(attributes)
   end
+end
